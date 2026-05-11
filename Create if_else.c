90017fc4fd5caabf30a927fb54aa35ae662b0540
Santiago Vivar <santiagovivar99@icloud.com>
2026-05-11 16:19:22 +0200

    Create Create if_else.c

---
+++ Create if_else.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    int number = 10;
+
+    if(number > 5) {
+        printf("Number is greater than 5");
+    } else {
+        printf("Number is 5 or smaller");
+    }
+
+    return 0;
+}
