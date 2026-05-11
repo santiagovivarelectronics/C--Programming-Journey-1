1ab9401ab9d5707df4e3c2dda7ffd1117482fa83
Santiago Vivar <santiagovivar99@icloud.com>
2026-05-11 16:19:46 +0200

    Create Create variables.c

---
+++ Create variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    int age = 21;
+    float height = 1.78;
+    char grade = 'A';
+
+    printf("Age: %d\n", age);
+    printf("Height: %.2f\n", height);
+    printf("Grade: %c\n", grade);
+
+    return 0;
+}
