6c0407127921554e3d098a86259a2adc10e44f8f
Santiago Vivar <santiagovivar99@icloud.com>
2026-05-11 12:33:20 +0200

    Create get input from a User.c

---
+++ get input from a User.c
@@ -0,0 +1,25 @@
+
+int main()
+{
+    //The User want to know his Grade in Math, he must type his name
+char NAME [20];
+char Grade = 'A';
+printf("Do you want to know your Grade at Math? ");
+
+printf("Please enter your Name: ");
+
+scanf("%s",&NAME);// With scanf(); you can get Input from the User.
+
+if (NAME = "Santiago");{
+
+        printf("Hello %s !",&NAME);
+
+printf("Your Grade is %c",Grade);
+
+printf("Have a Good Day!");
+
+}else (printf("Sorry i do not know you!  , Have a Good Day! "));
+
+
+return(0);
+}
