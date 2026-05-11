107ba9c9bce7c29ba2a49b577598017cf09d8e7a
Santiago Vivar <santiagovivar99@icloud.com>
2026-05-11 11:52:26 +0200

    Create constants.c

---
+++ constants.c
@@ -0,0 +1,13 @@
+#include <stdio.h>aa
+int main() {
+
+   const int num = 5;
+   printf("I have %d Jeahrs old. /n");
+int num = 8;// This variable is it not going to work because num is a constant
+
+   printf("My Dog have %d toys /n");
+
+  printf("My kid ate %d Apples/n");
+
+    return 0;
+}
