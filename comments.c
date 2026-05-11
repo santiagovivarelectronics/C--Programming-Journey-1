8bcdc698b02eaa8d0ddb129a34ac8417bc5dff35
Santiago Vivar <santiagovivar99@icloud.com>
2026-05-11 11:35:53 +0200

    Create comments.c

---
+++ comments.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    // This program prints text
+
+    printf("Hello Santiago!\n");
+
+    /*
+    Multi-line comment
+    Learning C programming
+    */
+
+    printf("Learning Embedded Systems!\n");
+
+    return 0;
+}
